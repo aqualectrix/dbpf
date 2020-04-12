ff02c5f19a945d95093d00701298d46481fa3522
aqualectrix <aqualectrix@gmail.com>
2020-04-12 12:03:15 -0600

    Create python equivalent to sorterMain.cpp

---
+++ aqualectrix/sorter/sortindex.py
@@ -0,0 +1,26 @@
+import argparse
+
+import sortProcessWrapper
+
+def main(args):
+    args = parse_args(args)
+
+    for f in args.filenames:
+        sortProcessWrapper.sortindexFile(f, args.hex_index)
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description = "Change the sortindex of the given files to the given hex value.")
+    parser.add_argument("hex_index", type=hex, help = "Value to apply to sortindex, in hex.")
+    parser.add_argument("filenames", help = "File(s) to process", nargs = "*")
+
+    return parser.parse_args(args);
+
+# hex type for argparsing hex_index
+def hex(hex_string):
+    value = int(hex_string, 16)
+    return value
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv[1:])
