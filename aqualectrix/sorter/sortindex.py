b26e0539bbd0e6ff11c5208161fce24b00ab58f1
aqualectrix <aqualectrix@gmail.com>
2020-04-21 17:29:48 -0600

    Add second operation mode, which sorts based on suffix and provided mapfile.

--- aqualectrix/sorter/sortindex.py
+++ aqualectrix/sorter/sortindex.py
@@ -1,4 +1,7 @@
 import sortProcessWrapper
+import mapReader
+
+from warnings import warn
 
 from gooey import Gooey
 from gooey import GooeyParser
@@ -6,13 +9,27 @@ from gooey import GooeyParser
 def main(args):
     args = parse_args(args)
 
-    for f in args.filenames:
-        sortProcessWrapper.sortindexFile(f, args.hex_index)
+    if args.index:
+        for f in args.filenames:
+            sortProcessWrapper.sortindexFile(f, args.hex_index)
+
+    if args.mapfile:
+        suffix_map = mapReader.parseMapFile(args.mapfile)
+        for f in args.filenames:
+            suffix = f.split("_")[-1].split(".")[0].casefold()
+            if not suffix_map[suffix]:
+                warn("Suffix " + suffix + " was not found in your map. " + f + " will not be sorted.")
+            else:
+                sortProcessWrapper.sortindexFile(f, suffix_map[suffix])
 
 @Gooey
 def parse_args(args):
     parser = GooeyParser(description = "Change the sortindex of the given files to the given hex value.")
-    parser.add_argument("hex_index", type=hex, help = "Value to apply to sortindex, in hex.")
+
+    indexGroup = parser.add_mutually_exclusive_group(required = True)
+    indexGroup.add_argument("-i", "--index", type=hex, help = "Value to apply to sortindex, in hex.")
+    indexGroup.add_argument("-m", "--mapfile", help = "File listing suffix:index pairs. Sortindex to apply is determined by the suffix (characters after the last _ in the file name) of the file.", widget = "FileChooser")
+
     parser.add_argument("filenames", help = "File(s) to process", nargs = "*", widget="MultiFileChooser")
 
     return parser.parse_args(args);
