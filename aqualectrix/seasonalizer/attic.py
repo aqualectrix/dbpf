881122bc51401382a0404f31be48673b1cb5ee98
aqualectrix <aqualectrix@gmail.com>
2020-04-23 18:41:59 -0600

    Added tests and fixed some logic problems revealed by tests.

--- aqualectrix/seasonalizer/attic.py
+++ aqualectrix/seasonalizer/attic.py
@@ -1,10 +1,15 @@
+import operator
 import os
 import shutil
+
+from functools import reduce
 
 Seasons = {"Spring": ["Spring"],
            "Summer": ["Summer"],
            "Fall/Autumn": ["Fall", "Autumn"],
            "Winter": ["Winter"]}
+
+SeasonSet = set(reduce(operator.add, Seasons.values()))
 
 def box(name, dir):
     '''
@@ -24,31 +29,34 @@ def unbox(file, location):
     shutil.unpack_archive(file, location, 'zip')
     os.remove(file)
 
+def isEligible(name):
+    '''
+    Returns true iff the name contains one of the entries in Seasons,
+    in any case.
+    '''
+    return any(seasonname.casefold() in name.casefold()
+               for seasonname in SeasonSet)
+
 def isSeasonable(season, name):
     '''
     Returns true if the name contains the season (in any case).
     'Fall' and 'Autumn' are equivalent.
     '''
-    # if season != "Fall/Autumn":
-    #     return season.casefold() in name.casefold()
-    # else:
-    #     return "fall".casefold() in name.casefold() or
-    #            "autumn".casefold() in name.casefold()
-
-    return any(seasonname.casefold() in name.casefold() for seasonname in Seasons[season])
+    return any(seasonname.casefold() in name.casefold()
+               for seasonname in Seasons[season])
 
 
 def pack_away_unseasonables(season, dir):
     '''
     Boxes up any subdirectories of dir that do *not* contain the season
-    in their names.
+    in their names (but do contain *some* season in their names).
     '''
     wd = os.getcwd()
     os.chdir(dir)
     try:
         with os.scandir(dir) as entries:
             for entry in entries:
-                if entry.is_dir() and isSeasonable(season, entry.name):
+                if entry.is_dir() and isEligible(entry.name) and not isSeasonable(season, entry.name):
                     box(entry.name, entry.name)
     finally:
         os.chdir(wd)
@@ -69,12 +77,15 @@ def unpack_seasonables(season, dir):
         os.chdir(wd)
 
 # Tests
-# This is just an experimentation area to ensure I understand how
-# make_archive / unpack_arcive work; there's not really any non-built-in
-# functionality to test.
 import unittest
 
-class TestAtticStorage(unittest.TestCase):
+class TestAtticBoxes(unittest.TestCase):
+    '''
+    Basically just an experimentation area to ensure I understand how
+    make_archive / unpack_archive work; there's not really any non-built-in
+    functionality to test for box/unbox.
+    '''
+
     TEST_TMP = "test_tmp"
     PACKABLE_DIR = "numbers"
 
@@ -120,11 +131,163 @@ class TestAtticStorage(unittest.TestCase):
         os.chdir(os.pardir)
         shutil.rmtree(cls.TEST_TMP)
 
-class TestAtticUnbox(unittest.TestCase):
+class TestAtticNameChecks(unittest.TestCase):
+    '''
+    Test isEligible() and isSeasonable().
+    '''
 
-    def setUpClass(self):
-        pass
+    def test_isEligibleRejectsNonSeasonables(self):
+        self.assertFalse(isEligible("Nonsense name"))
 
+    def test_isEligibleIgnoresCase(self):
+        self.assertTrue(isEligible("SPRING"))
+
+    def test_isEligibleAcceptsSeasonables(self):
+        self.assertTrue(isEligible("FALL"))
+
+    def test_isSeasonableRejectsIneligibles(self):
+        self.assertFalse(isSeasonable("Spring", "Nonsense name"))
+
+    def test_isSeasonableIgnoresCase(self):
+        self.assertTrue(isSeasonable("Spring", "SPRING"))
+
+    def test_isSeasonableHandlesFallAndAutumn(self):
+        self.assertTrue(isSeasonable("Fall/Autumn", "Fall"))
+        self.assertTrue(isSeasonable("Fall/Autumn", "Autumn"))
+
+class TestAtticPacking(unittest.TestCase):
+    '''
+    Tests packing unseasonables and unpacking seasonables.
+    '''
+    TEST_TMP = "season_tmp"
+
+    def test_packDoesNotPackStrayFiles(self):
+        season = "Summer"
+        stray = "Just some file.txt"
+        with open(stray, "w") as f:
+            f.write("Single files don't get packed.")
+
+        pack_away_unseasonables(season, os.curdir)
+
+        self.assertFalse(isEligible(stray))
+        self.assertTrue(os.path.exists(stray))
+        self.assertFalse(os.path.exists(stray + ".zip"))
+
+    def test_packDoesNotPackStrayUnseasonableFiles(self):
+        season = "Summer"
+        unseasonable = "Spring.txt"
+        with open(unseasonable, "w") as f:
+            f.write("Single files, even if unseasonable, don't get packed.")
+
+        pack_away_unseasonables(season, os.curdir)
+
+        self.assertTrue(isEligible(unseasonable))
+        self.assertFalse(isSeasonable(season, unseasonable))
+        self.assertTrue(os.path.exists(unseasonable))
+        self.assertFalse(os.path.exists(unseasonable + ".zip"))
+
+    def test_packDoesNotPackUninvolvedFolders(self):
+        season = "Summer"
+        uninvolved = "All"
+        os.mkdir(uninvolved)
+        with open(os.path.join(uninvolved, uninvolved + ".txt"), "w") as f:
+            f.write("This file's folder shouldn't be touched since it has no season names in it.")
+
+        pack_away_unseasonables(season, os.curdir)
+
+        self.assertFalse(isEligible(uninvolved))
+        self.assertTrue(os.path.exists(uninvolved))
+        self.assertFalse(os.path.exists(uninvolved + ".zip"))
+
+    def test_unpackDoesNotUnpackUninvolvedZips(self):
+        season = "Summer"
+        uninvolved = "Backups.zip"
+        with open(uninvolved, "w") as f:
+            f.write("This is not a real zipfile :)")
+
+        unpack_seasonables(season, os.curdir)
+
+        self.assertFalse(isEligible(uninvolved))
+        self.assertFalse(isSeasonable(season, uninvolved)) # trivial since ineligible
+        self.assertTrue(os.path.exists(uninvolved))
+        self.assertFalse(os.path.exists(uninvolved[:-4]))
+
+    def test_packDoesNotPackSeasonableFolders(self):
+        season = "Summer"
+        seasonable = season + " + Spring"
+        os.mkdir(seasonable)
+        with open(os.path.join(seasonable, seasonable + ".txt"), "w") as f:
+            f.write("This file's folder shouldn't be touched since it's seasonable.")
+
+        pack_away_unseasonables(season, os.curdir)
+
+        self.assertTrue(isEligible(seasonable))
+        self.assertTrue(isSeasonable(season, seasonable))
+        self.assertTrue(os.path.exists(seasonable))
+        self.assertFalse(os.path.exists(seasonable + ".txt"))
+
+    def test_unpackUnpacksSeasonableZips(self):
+        season = "Summer"
+        seasonable = season + " + Fall"
+        os.mkdir(seasonable)
+        with open(os.path.join(seasonable, seasonable + ".txt"), "w") as f:
+            f.write("Summer & Fall")
+        box(seasonable, seasonable)
+
+        unpack_seasonables(season, os.curdir)
+
+        self.assertTrue(isEligible(seasonable))
+        self.assertTrue(isSeasonable(season, seasonable))
+        self.assertTrue(os.path.exists(seasonable))
+        self.assertFalse(os.path.exists(seasonable + ".zip"))
+
+    def test_packPacksUnseasonableFolders(self):
+        season = "Summer"
+        unseasonable = "Fall & Winter"
+        os.mkdir(unseasonable)
+        with open(os.path.join(unseasonable, unseasonable + ".txt"), "w") as f:
+            f.write("This file's folder should get packed!")
+
+        pack_away_unseasonables(season, os.curdir)
+
+        self.assertTrue(isEligible(unseasonable))
+        self.assertFalse(isSeasonable(season, unseasonable))
+        self.assertFalse(os.path.exists(unseasonable))
+        self.assertTrue(os.path.exists(unseasonable + ".zip"))
+
+    def test_unpackDoesNotUnpackUnseasonableZips(self):
+        season = "Summer"
+        unseasonable = "Fall & Winter.zip"
+        with open(unseasonable, "w") as f:
+            f.write("This is not a real zip file.")
+
+        unpack_seasonables(season, os.curdir)
+
+        self.assertTrue(isEligible(unseasonable))
+        self.assertFalse(isSeasonable(season, unseasonable))
+        self.assertTrue(os.path.exists(unseasonable))
+        self.assertFalse(os.path.exists(unseasonable[:-4]))
+
+    @classmethod
+    def setUpClass(cls):
+        # Add directory for tests to play in
+        os.mkdir(cls.TEST_TMP)
+        os.chdir(cls.TEST_TMP)
+
+    def tearDown(self):
+        # Remove any manipulations, leaving TEST_TMP empty
+        with os.scandir(os.curdir) as contents:
+            for entry in contents:
+                if os.path.isfile(entry):
+                    os.remove(entry)
+                else:
+                    shutil.rmtree(entry)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Remove test_tmp directory
+        os.chdir(os.pardir)
+        shutil.rmtree(cls.TEST_TMP)
 
 # Main; runs tests
 if __name__ == "__main__":
